c4223899ac7fdb311a7674e398ab5bfba398dd72
marco-rasori <marco.rasori@gmail.com>
2023-03-30 13:14:26 +0200

    added hazards to the request. All of them have AttributeID ...:environment:hazards. The request contains only the names of the hazards, we need to figure out how to include the risk values. Perhaps, with XACML structured attributes? It might be feasible for the requests, but on the other hand, the XACML engine (Balana) seems not to support structured attributes.

--- src/bin/sifis-xacml.rs
+++ src/bin/sifis-xacml.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use manifest::AppLabel;
+use manifest::{AppLabel, Hazard};
 
 fn read_app_label_from_file<P: AsRef<Path>>(path: P) -> Result<AppLabel, Box<dyn Error>> {
 
@@ -16,7 +16,7 @@ fn read_app_label_from_file<P: AsRef<Path>>(path: P) -> Result<AppLabel, Box<dyn
     Ok(app_label)
 }
 
-fn create_request(app_name: &str, device_type: &str, action: &str) -> String {
+fn create_request(app_name: &str, device_type: &str, action: &str, hazards: Vec<&Hazard>) -> String {
     let start_request : String = r#"
     <Request xmlns="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17" CombinedDecision="false" ReturnPolicyIdList="false" >"#.to_owned();
 
@@ -47,20 +47,28 @@ fn create_request(app_name: &str, device_type: &str, action: &str) -> String {
       </Attribute>
     </Attributes>"#.to_owned();
 
-    // TODO: add the attributes related to the hazards
-    // <Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:environment">
-    // <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:environment:hazards" IncludeInResult="false">
-    // <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">FireHazard</AttributeValue>
-    // <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">LogEnergyConsumption</AttributeValue>
-    // <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">ElectricEnergyConsumption</AttributeValue>
-    // </Attribute>
-    // </Attributes>
+    // Let's start by adding just the hazards
+    // TODO: manage the risk score
+    let mut environment: String = r#"
+    <Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:environment">
+      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:environment:hazards" IncludeInResult="false">"#.to_owned();
+
+    for hazard in hazards {
+        let hazard_attribute : String = r#"
+        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">"#.to_owned() + hazard.name.as_str() + r#"</AttributeValue>"#;
+        environment = format!("{}{}", environment, hazard_attribute);
+    }
+
+    let end_environment : String = r#"
+      </Attribute>
+    </Attributes>"#.to_owned();
+    environment = format!("{}{}", environment, end_environment);
 
     let end_request : String = r#"
     </Request>"#.to_owned();
 
-    let request = format!("{}{}{}{}{}",
-                           start_request, subject, resource, action, end_request);
+    let request = format!("{}{}{}{}{}{}",
+                           start_request, subject, resource, action, environment, end_request);
 
     return request;
 }
@@ -78,21 +86,33 @@ fn main() {
         let device_type : &str = api_label.behavior_label[0].device_type.as_str();
         let action : &str = api_label.behavior_label[0].action.as_str();
 
-        // TODO: extract the hazards
-        // let mut hazards : Vec<Hazard> = Vec::new();
-        // for safety_hazard in api_label.security_label.safety.iter() {
-        //     // get the hazard name and optional risk score
-        //     hazards.push(safety_hazard)
-        // }
+        // create an array containing all the hazards of this API label
+        let num_hazards = api_label.security_label.safety.capacity() +
+            api_label.security_label.privacy.capacity() +
+            api_label.security_label.financial.capacity();
+
+        let mut hazards = Vec::with_capacity(num_hazards);
+
+        for safety_hazard in api_label.security_label.safety.iter() {
+            hazards.push(safety_hazard);
+        }
+
+        for privacy_hazard in api_label.security_label.privacy.iter() {
+            hazards.push(privacy_hazard);
+        }
+
+        for financial_hazard in api_label.security_label.financial.iter() {
+            // get the hazard name and optional risk score
+            hazards.push(financial_hazard)
+        }
 
         // create an xacml request for the current API label
         // and add it to the array of the requests
-        requests.push(create_request(app_name, device_type, action));
+        requests.push(create_request(app_name, device_type, action, hazards));
     }
 
     // print each extracted request as a string
     for value in requests.iter() {
         println!("{}", value);
     }
-
 }
