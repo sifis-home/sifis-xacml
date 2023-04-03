21566b818cda001656cd0cb1656eafc6324cfa27
marco-rasori <marco.rasori@gmail.com>
2023-04-03 18:13:20 +0200

    Add template and command line arguments.
    Minijinja is now used as dependency; each XACML request is created by using the template request.xml, and it is populated with information contained in the app label

--- src/bin/sifis-xacml.rs
+++ src/bin/sifis-xacml.rs
@@ -1,8 +1,20 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Write};
 use std::path::Path;
-use manifest::{AppLabel, Hazard};
+use std::path::PathBuf;
+
+use clap::Parser;
+
+use manifest::AppLabel;
+
+use minijinja::{context, Environment};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Context {
+    name: String,
+}
 
 fn read_app_label_from_file<P: AsRef<Path>>(path: P) -> Result<AppLabel, Box<dyn Error>> {
 
@@ -16,103 +28,65 @@ fn read_app_label_from_file<P: AsRef<Path>>(path: P) -> Result<AppLabel, Box<dyn
     Ok(app_label)
 }
 
-fn create_request(app_name: &str, device_type: &str, action: &str, hazards: Vec<&Hazard>) -> String {
-    let start_request : String = r#"
-    <Request xmlns="urn:oasis:names:tc:xacml:3.0:core:schema:wd-17" CombinedDecision="false" ReturnPolicyIdList="false" >"#.to_owned();
+#[derive(Parser, Debug)]
+struct Opts {
+    /// Path to the JSON file containing the App Label
+    #[clap(short, value_parser)]
+    app_label_path: PathBuf,
 
-    let subject : String = r#"
-	<Attributes Category="urn:oasis:names:tc:xacml:1.0:subject-category:access-subject">
-	  <Attribute IncludeInResult="false" AttributeId="urn:oasis:names:tc:xacml:1.0:subject:subject-id">
-	    <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">manifest</AttributeValue>
-	  </Attribute>
-    </Attributes>"#.to_owned();
+    /// Output path of the generated XACML requests.
+    /// Each request file is saved with the name "request_\<x\>.xml",
+    /// where \<x\> is an increasing integral starting from 1.
+    #[clap(short, value_parser)]
+    output_path: Option<PathBuf>,
 
-    let resource : String = r#"
-    <Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:resource">
-      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:resource-id" IncludeInResult="false">
-        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">"#.to_owned() + app_name + r#"</AttributeValue>
-      </Attribute>
-      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:device-type" IncludeInResult="false">
-        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">"# + device_type + r#"</AttributeValue>
-      </Attribute>
-      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:resource:action" IncludeInResult="false">
-        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">"# + action + r#"</AttributeValue>
-      </Attribute>
-    </Attributes>"#;
+    /// Enable additional information about the underlying process
+    #[clap(short, long)]
+    verbose: bool,
 
-    let action : String = r#"
-    <Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:action">
-      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:action:action-id" IncludeInResult="false">
-        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">install</AttributeValue>
-      </Attribute>
-    </Attributes>"#.to_owned();
-
-    // Let's start by adding just the hazards
-    // TODO: manage the risk score
-    let mut environment: String = r#"
-    <Attributes Category="urn:oasis:names:tc:xacml:3.0:attribute-category:environment">
-      <Attribute AttributeId="urn:oasis:names:tc:xacml:1.0:environment:hazards" IncludeInResult="false">"#.to_owned();
-
-    for hazard in hazards {
-        let hazard_attribute : String = r#"
-        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">"#.to_owned() + hazard.name.as_str() + r#"</AttributeValue>"#;
-        environment = format!("{}{}", environment, hazard_attribute);
-    }
-
-    let end_environment : String = r#"
-      </Attribute>
-    </Attributes>"#.to_owned();
-    environment = format!("{}{}", environment, end_environment);
-
-    let end_request : String = r#"
-    </Request>"#.to_owned();
-
-    let request = format!("{}{}{}{}{}{}",
-                           start_request, subject, resource, action, environment, end_request);
-
-    return request;
+    /// Enable additional information about the underlying process
+    #[clap(short, long)]
+    save: bool,
 }
 
-fn main() {
-    let app_label = read_app_label_from_file("data/app_label.json").unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
 
-    println!("Extracting XACML requests from app: \"{}\"...",app_label.app_name);
+    let opts = Opts::parse();
 
-    let mut requests: Vec<String> =  Vec::new();
+    //let app_label = read_app_label_from_file("data/app_label.json").unwrap();
+    let app_label = read_app_label_from_file(opts.app_label_path)?;
 
-    // from each API label, we create an XACML request
-    for api_label in app_label.api_labels {
-        let app_name : &str = app_label.app_name.as_str();
-        let device_type : &str = api_label.behavior_label[0].device_type.as_str();
-        let action : &str = api_label.behavior_label[0].action.as_str();
+    let mut env = Environment::new();
+    env.add_template(
+        "request.xml",
+        include_str!("../../templates/request.xml"))
+        .unwrap();
 
-        // create an array containing all the hazards of this API label
-        let num_hazards = api_label.security_label.safety.capacity() +
-            api_label.security_label.privacy.capacity() +
-            api_label.security_label.financial.capacity();
+    let tmpl = env.get_template("request.xml").unwrap();
 
-        let mut hazards = Vec::with_capacity(num_hazards);
+    println!("Creating XACML requests from app: \"{}\"...",app_label.app_name);
 
-        for safety_hazard in api_label.security_label.safety.iter() {
-            hazards.push(safety_hazard);
+    for idx in 0..app_label.api_labels.len() {
+        let req = tmpl.render(context!(
+            app_label,
+            index => idx,
+        )).unwrap();
+
+        if opts.verbose {
+            println!("{}", req);
         }
 
-        for privacy_hazard in api_label.security_label.privacy.iter() {
-            hazards.push(privacy_hazard);
+        if opts.save {
+            let mut file = File::create(
+                format!("request_{}.xml", idx + 1))?;
+            file.write_all(req.as_ref())?;
         }
-
-        for financial_hazard in api_label.security_label.financial.iter() {
-            // get the hazard name and optional risk score
-            hazards.push(financial_hazard)
-        }
-
-        // create an xacml request for the current API label
-        // and add it to the array of the requests
-        requests.push(create_request(app_name, device_type, action, hazards));
     }
 
-    // print each extracted request as a string
-    for value in requests.iter() {
-        println!("{}", value);
+    print!("XACML requests created successfully");
+    if opts.save {
+        println!(" and saved to ...");
     }
+
+    Ok(())
 }
